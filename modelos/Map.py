bdd7eefd4df36bf107352d3284ab42b6831a1b87
clanie1 <danybarociov@gmail.com>
2022-12-06 23:23:06 -0800

    ya no hay error

--- modelos/Map.py
+++ modelos/Map.py
@@ -28,7 +28,7 @@ class Map:
     self.evento = Evento(lat, long)
     
   #
-  def getDoubleDistance(distancia):
+  def getDoubleDistance(self,distancia):
     nums = ['0','1','2','3','4','5','6','7','8','9','.']
     i = 0
     finalNum = ""
