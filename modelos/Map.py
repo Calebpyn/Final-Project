01f2ff6430a71bdc900a8b57b72cfa3c519cfa3b
Adrián Fernández <oscar.adrian.fersa@gmail.com>
2022-12-06 20:34:13 -0800

    no tengo ni perra idea de como funciona la indentacion en perro python

--- modelos/Map.py
+++ modelos/Map.py
@@ -3,10 +3,10 @@ from Evento import Evento
 import requests
 
 class Map:
-    patrullas = []
-    evento = None
+  patrullas = []
+  evento = None
 
-	def __init__(self):
+  def __init__(self):
     num = int(input("Ingrese el numero de patrullas: "))
     arr = []
     for i in range(num):
@@ -24,24 +24,25 @@ class Map:
     long = float(input("Ingrese la longitud: "))
     self.evento = Evento(lat, long)
 
-	def distancePatrullaEvento(self, patrullas, evento):
-		patrullaDistancia = {}
-		for i in range(len(patrullas)):
-			url = f"https://maps.googleapis.com/maps/api/directions/json?origin={patrullas[i].lat},{patrullas[i].long}&destination={evento.lat},{evento.long}&key=AIzaSyCu6N_Jv75wSs8TnytapVPub3oSycUPBzk"
-			payload={}
-			headers = {}
-			response = requests.request("GET", url, headers=headers, data=payload)
-			locacionEvento = response.json()["routes"][0]["legs"][0]["end_location"]
-			carroPolicia = response.json()["routes"][0]["legs"][0]["start_location"]
-			distance = response.json()["routes"][0]["legs"][0]["distance"]["text"]
-		patrullaDistancia[patrullas[i]] = distance	
-		print(patrullaDistancia)
+  def distancePatrullaEvento(self, patrullas, evento):
+    patrullaDistancia = {}
+    for i in range(len(patrullas)):
+      url = f"https://maps.googleapis.com/maps/api/directions/json?origin={patrullas[i].lat},{patrullas[i].long}&destination={evento.lat},{evento.long}&key=AIzaSyCu6N_Jv75wSs8TnytapVPub3oSycUPBzk"
+      payload={}
+      headers = {}
+      response = requests.request("GET", url, headers=headers, data=payload)
+      locacionEvento = response.json()["routes"][0]["legs"][0]["end_location"]
+      carroPolicia = response.json()["routes"][0]["legs"][0]["start_location"]
+      distance = response.json()["routes"][0]["legs"][0]["distance"]["text"]
+      patrullaDistancia[patrullas[i]] = distance
+      
+      print(patrullaDistancia)
         
     #INCOMPLETO
   def atenciondeEvento(self, distancias):
     #aqui es donde definimos que patrulla es la que va a atender el evento
     if self.evento == None:
-    	print("no se ha creado un evento")
+      print("no se ha creado un evento")
     return 0
 
     #INCOMPLETO
